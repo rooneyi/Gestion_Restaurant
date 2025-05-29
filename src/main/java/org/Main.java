3ddbf00d424033b9f3fcdd1d4246029278bfada2
#Médard... <21mk367@esisalama.org>
2025-05-29 06:00:38 -0800

    customiser la page d'accueil

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
+
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/auth.fxml"));
         Parent root = fxmlLoader.load();
