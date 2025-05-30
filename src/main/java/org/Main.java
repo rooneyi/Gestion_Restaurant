6f1a14aa592880946c73dce60df5b41a739a8c34
#Médard... <21mk367@esisalama.org>
2025-05-30 13:39:53 -0800

    ajustement avec un fichier CSS (styleDashBorad.css)

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     double x,y = 0;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/views/dashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Auth.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         root.setOnMousePressed(event -> {
@@ -24,7 +24,7 @@ public class Main extends Application {
             primaryStage.setY(event.getScreenY() - y);
         });
 
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 1150, 660));
         primaryStage.show();
     }
 
